e0037affa1aa58540b4623e6bd5fb72ae593a72e
Patrick Powell <prp1277@gmail.com>
2019-01-29 08:56:35 -0600

    refactor from football to to-do

--- src/App.js
+++ src/App.js
@@ -1,42 +1,16 @@
-import React from 'react';
-import logo from './favicon.png';
-import './App.css';
-import Odds from './data/Odds';
+import React from "react";
+import ReactDOM from "react-dom";
 
+import "./styles.css";
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      odds1: {
-        type: `sr:sport:16`,
-        date: `DTNow`,
-      },
-    }
-  }
-  render() {
-    return (
-      <div id="root" className="container-fluid bg-transparent">
-
-        <body id="App">
-
-          <section id="navbar">
-            <nav className="navbar navbar-dark bg-secondary justify-content-center">
-              <a className="nav-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-                <img src={logo} className="navbar-brand" alt="logo" />
-              </a>
-            </nav>
-          </section>
-
-          <article id="main">
-            <Odds />
-          </article>
-
-        </body>
-
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello CodeSandbox</h1>
+      <h2>Start editing to see some magic happen!</h2>
+    </div>
+  );
 }
 
-export default App;
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
--- src/components/Error.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const Error = () => (
-  <div>
-    <h1>You Done Fucked Something Up</h1>
-  </div>
-);
-
-export default Error;
