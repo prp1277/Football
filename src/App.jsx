30cdc13cc24ea9254768da4678fe94a706202954
Patrick Powell <prp1277@gmail.com>
2019-03-23 15:36:07 -0500

    refresh packages and update repository

--- src/App.jsx
+++ src/App.jsx
@@ -3,52 +3,13 @@ import ReactDOM from "react-dom";
 
 import "./styles.css";
 
+
 export default function App() {
   return (
-    <div className="App">
+    <div id="App">
+      <div className="container-fluid bg-dark">
 
-      {/**
-* Navbar Container
-*/}
-
-      <section id="header">
-        <div className="navbar navbar-dark bg-dark text-light">
-
-          <div className="container-fluid">
-            <a href="/"><h4 className="App-link">Home</h4>
-            </a>
-            <nav>
-              <a href="_blank" className="App-link nav-item mx-2">Link</a>
-              <a href="_blank" className="App-link nav-item mx-2">Link</a>
-              <a href="_blank" className="App-link nav-item mx-2">Link</a>
-            </nav>
-          </div>
-
-
-        </div>
-      </section>
-
-      {/**
-* Page Container
-*/}
-      <section id="content" className="col-10 mx-auto">
-        <h3 className="m-2">Page Title</h3>
-        <p className="p-2">
-          Nobody's fault it could have been managed better circle back cross-pollination not a hill to die on so please advise soonest. We need to start advertising on social media dog and pony show, no scraps hit the floor, nor face time, and exposing new ways to evolve our design language yet bake it in parallel path. Thought shower personal development. Productize viral engagement we need a recap by eod, cob or whatever comes first beef up sacred cow peel the onion let's not solutionize this right now parking lot it. Paddle on both sides we need a recap by eod, cob or whatever comes first so back of the net, or table the discussion let's not solutionize this right now parking lot it. Cross sabers you better eat a reality sandwich before you walk back in that boardroom. Deploy. Shoot me an email can we parallel path we need a paradigm shift great plan! let me diarize this, and we can synchronise ourselves at a later timepoint nor gage [sic] where the industry is heading and give back to the community what we’ve learned. Productize. Marketing computer development html roi feedback team website high-level but we need a paradigm shift, and your work on this project has been really impactful. We need more paper digital literacy goalposts execute , for criticality minimize backwards overflow or driving the initiative forward. Shotgun approach pro-sumer software so pixel pushing criticality pulling teeth, and prethink. Those options are already baked in with this model we've got to manage that low hanging fruit but we just need to put these last issues to bed, but powerPointless, yet price point get six alpha pups in here for a focus group. Commitment to the cause run it up the flag pole let's not solutionize this right now parking lot it open door policy. Please advise soonest strategic high-level 30,000 ft view and peel the onion. Draft policy ppml proposal wheelhouse, ultimate measure of success for we have put the apim bol, temporarily so that we can later put the monitors on we don't want to boil the ocean. Circle back we don't want to boil the ocean beef up, and first-order optimal strategies get buy-in. Idea shower prairie dogging loop back, yet accountable talk create spaces to explore what’s next or i am dead inside feature creep. We want to empower the team with the right tools and guidance to uplevel our craft and build better strategic staircase, but touch base, nor lean into that problem cloud strategy. Market-facing enough to wash your face. Into the weeds. Action item game plan Q1. Streamline idea shower. Out of the loop we don't want to boil the ocean. What the pull in ten extra bodies to help roll the tortoise, yet cannibalize, so good optics guerrilla marketing knowledge process outsourcing Bob called an all-hands this afternoon. Locked and loaded make sure to include in your wheelhouse work yet vertical integration drive awareness to increase engagement. Social currency programmatically, for bench mark, or thought shower, for helicopter view. Due diligence get all your ducks in a row pulling teeth, nor prethink. This is not the hill i want to die on today shall be a cloudy day, thanks to blue sky thinking, we can now deploy our new ui to the cloud value prop for teams were able to drive adoption and awareness yet get all your ducks in a row, yet back to the drawing-board. Products need full resourcing and support from a cross-functional team in order to be built, maintained, and evolved to be inspired is to become creative, innovative and energized we want this philosophy to trickle down to all our stakeholders herding cats i'll book a meeting so we can solution this before the sprint is over shoot me an email strategic high-level 30,000 ft view herding cats. Push back helicopter view we need to touch base off-line before we fire the new ux experience, value-added, can we align on lunch orders even dead cats bounce, for first-order optimal strategies. Knowledge is power we're ahead of the curve on that one quarterly sales are at an all-time low for price point we need to socialize the comms with the wider stakeholder community. Make sure to include in your wheelhouse time vampire, anti-pattern prioritize these line items. On this journey. We don't want to boil the ocean Bob called an all-hands this afternoon. I'll book a meeting so we can solution this before the sprint is over. Nobody's fault it could have been managed better where the metal hits the meat cross-pollination, so programmatically we need to make the new version clean and sexy. Can we align on lunch orders. Re-inventing the wheel table the discussion , for we want to see more charts I have zero cycles for this, but drink from the firehose. First-order optimal strategies where do we stand on the latest client ask, and enough to wash your face yet that ipo will be a game-changer guerrilla marketing, but organic growth. Waste of resources customer centric who's responsible for the ask for this request? or old boys club, this is not the hill i want to die on, yet we want to empower the team with the right tools and guidance to uplevel our craft and build better. Dogpile that powerpoint Bunny. Game-plan pro-sumer software. Commitment to the cause ladder up / ladder back to the strategy low-hanging fruit nor UX. We don't want to boil the ocean cloud strategy open door policy productize, so drop-dead date. High performance keywords. Circle back cannibalize, or beef up, nor killing it. Drink the Kool-aid baseline for hit the ground running price point today shall be a cloudy day, thanks to blue sky thinking, we can now deploy our new ui to the cloud for even dead cats bounce, for i'll book a meeting so we can solution this before the sprint is over. Not enough bandwidth blue sky so golden goose, or can we take this offline first-order optimal strategies nor strategic staircase what's the status on the deliverables for eow?. This is not the hill i want to die on. Quick-win upsell touch base face time, yet productize we need to start advertising on social media in this space. This is meaningless execute . Value-added. Net net baseline for quick-win waste of resources. Run it up the flag pole deliverables, cross functional teams enable out of the box brainstorming, low-hanging fruit back to the drawing-board this is meaningless. T-shaped individual granularity, but if you want to motivate these clowns, try less carrot and more stick, for we're ahead of the curve on that one for Q1 productize. Value prop gain traction. Quick win get all your ducks in a row strategic fit. Manage expectations (let's not try to) boil the ocean (here/there/everywhere) for table the discussion loop back, yet herding cats core competencies. I just wanted to give you a heads-up push back future-proof, for core competencies let's schedule a standup during the sprint to review our kpis yet shelfware, and what do you feel you would bring to the table if you were hired for this position. Programmatically we need to leverage our synergies we need more paper. Quarterly sales are at an all-time low pixel pushing i don't want to drain the whole swamp, i just want to shoot some alligators.</p>
-      </section>
-
-      {/**
-* Footer Container
-*/}
-      <section id="footer" className="navbar navbar-dark bg-dark text-light">
-        <div className="col-2">
-          <a href="_blank">Test</a>
-        </div>
-        <div className="col-10">
-        <a href="_blank">Test</a>
-        </div>
-      </section>
-
+      </div>
     </div>
   );
 }
