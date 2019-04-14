546c9e5babe50513ecad2f910c584b3a642f19d9
Patrick Powell <prp1277@gmail.com>
2019-04-14 09:55:38 -0500

    Simple interface for SEO

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,30 @@ import React, { Component } from 'react';
 import './styles.css';
 
 class App extends Component {
-  state = { count: 0 };
-  handleCount(value) {
-    this.setState((prevState) => {
-      return ({ count: prevState.count + value });
-    });
-  }
   render() {
     return (
-      <div>
-        <iframe width="933" height="700" src="https://app.powerbi.com/view?r=eyJrIjoiODE2MmRiZDYtZTI2Ny00MzI5LWI5ZjktMzllNDQ5NDBkNDNjIiwidCI6ImY1MmY5MWI0LTc1YzAtNGUwZC04MjkzLTZhYjQ5ZTViZjgwZiJ9" frameborder="0" allowFullScreen="true"></iframe>
+      <div id="App" className="App bg-light">
+        <div id="Header" className="navbar navbar-dark bg-dark">
+          <a href="/"><button className="btn btn-info">Home</button></a>
+        </div>
+
+        <div id="Body" className="container">
+          <div className="Page row text-center bg-light">
+
+            <div className="media container-fluid my-2">
+              <div className="media-body">
+                <img className="media-object" src="https://avatars0.githubusercontent.com/u/36834793?s=460&v=4" alt="" />
+                <h4 className="media-title">Patrick Powell</h4>
+                <p className="media-content">Celebrating Alex's wedding day</p>
+              </div>
+            </div>
+          </div>
+
+        </div>
+        <div id="Footer" className="navbar navbar-dark bg-dark">
+          <a href="/"><button className="btn btn-info">Home</button></a>
+        </div>
+
       </div>
     )
   }
