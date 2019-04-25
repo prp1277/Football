fc5602785a51d916520631126b100f327cb92179
Patrick Powell <prp1277@gmail.com>
2019-04-25 13:57:26 -0500

    PBI iframe - ESRI broken

--- src/App.jsx
+++ src/App.jsx
@@ -5,33 +5,35 @@ class App extends Component {
   render() {
     return (
       <div id="App" className="App bg-light">
+
         <div id="Header" className="navbar navbar-dark bg-dark">
-          <a href="/"><button className="btn btn-link">Home</button></a>
+          <img className="navbar-brand" src="https://github.com/prp1277/Pay-LESS/raw/master/payless-webstores/Pay-LESS.png" alt="" />
         </div>
 
-        <div className="container-fluid bg-secondary btn-group py-2">
+        <div id="Body" className="container text-center">
 
-          <button id="linkedin" className="btn btn-primary mx-2"><a className="text-light" href="https://linkedin.com/in/prp1277" >LinkedIn</a></button>
-          <button id="resume" className="btn btn-primary mx-2"><a className="text-light" href="https://1drv.ms/w/s!AoLkNBOSNnKylb1v8s3pCQn2LR-f-g" >Resume</a></button>
-          <button id="github" className="btn btn-primary mx-2"><a className="text-light" href="https://github.com/prp1277" >GitHub</a></button>
-          <button id="twitter" className="btn btn-primary mx-2"><a className="text-light" href="https://twitter.com/prp1277" >Twitter</a></button>
-          <button id="reddit" className="btn btn-primary mx-2"><a className="text-light" href="https://reddit.com/u/patp0w" >Reddit</a></button>
+          <div>
+            <h4>Kansas City</h4>
+            <iframe title="KansasCity" width="933" height="700" src="https://app.powerbi.com/view?r=eyJrIjoiZDA1NWYwNzktMjE1Ny00MTkxLWE0NTctYjc5ZWE0MjU2Njk5IiwidCI6ImY1MmY5MWI0LTc1YzAtNGUwZC04MjkzLTZhYjQ5ZTViZjgwZiJ9" frameborder="0" allowFullScreen="true"></iframe><br />
+          </div>
 
-        </div>
+          <div>
+            <h4>ESRI-DBConnection File</h4>
+            <iframe title="ESRI-DBConnection" width="933" height="700" src="https://app.powerbi.com/view?r=eyJrIjoiMGRmNjAxOGUtMjU2ZS00ZGZmLTkyZmYtODBkODNkNGExYzI1IiwidCI6ImY1MmY5MWI0LTc1YzAtNGUwZC04MjkzLTZhYjQ5ZTViZjgwZiJ9" frameborder="0" allowFullScreen="true"></iframe><br />
+          </div>
 
-        <div id="Body" className="container">
-          <div className="Page row text-center bg-light">
+          <div>
+            <h4>Des Moines Chains</h4>
+            <iframe title="DesMoinesChains" width="933" height="700" src="https://app.powerbi.com/view?r=eyJrIjoiMzZlMTRjMjktZGViMi00ODNlLWE2NTQtMzg0NTFmN2NhY2U3IiwidCI6ImY1MmY5MWI0LTc1YzAtNGUwZC04MjkzLTZhYjQ5ZTViZjgwZiJ9" frameborder="0" allowFullScreen="true"></iframe><br />
+          </div>
 
-            <div className="media container-fluid my-2">
-              <div className="media-body">
-                <img className="media-object" src="https://avatars0.githubusercontent.com/u/36834793?s=460&v=4" alt="" />
-                <h4 className="media-title">Patrick Powell</h4>
-                <p className="media-content">Celebrating Alex's wedding day</p>
-              </div>
-            </div>
+          <div>
+            <h4>Des Moines Clusters</h4>
+            <iframe title="DesMoines-Chains" width="933" height="700" src="https://app.powerbi.com/view?r=eyJrIjoiZWJmNjIxMjEtNjc5NS00ZWEwLTgxOTEtMTYzZGNhZDQzNmE4IiwidCI6ImY1MmY5MWI0LTc1YzAtNGUwZC04MjkzLTZhYjQ5ZTViZjgwZiJ9" frameborder="0" allowFullScreen="true"></iframe><br />
           </div>
 
         </div>
+
         <div id="Footer" className="navbar navbar-dark bg-dark">
           <a href="/"><button className="btn btn-link">Home</button></a>
         </div>
