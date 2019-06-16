caa2c34064bc570285f24b2266ba46723c7d32f8
Patrick Powell <prp1277@gmail.com>
2019-06-16 16:22:32 -0500

    Trial bitly link

--- src/components/SocialMedia.jsx
+++ src/components/SocialMedia.jsx
@@ -7,7 +7,7 @@ class SocialMedia extends Component {
       <div id="SocialMedia">
         <div className="row">
           <div className="container-fluid py-2">
-            <button id="linkedin" className="btn btn-primary mx-2"><a className="text-light" href="https://linkedin.com/in/prp1277" >LinkedIn</a></button>
+            <button id="linkedin" className="btn btn-primary mx-2"><a className="text-light" href="https://bit.ly/powell-linkedin" >LinkedIn</a></button>
             <button id="resume" className="btn btn-primary mx-2"><a className="text-light" href="https://1drv.ms/w/s!AoLkNBOSNnKylb1v8s3pCQn2LR-f-g" >Resume</a></button>
             <button id="github" className="btn btn-primary mx-2"><a className="text-light" href="https://github.com/prp1277" >GitHub</a></button>
             <button id="twitter" className="btn btn-primary mx-2"><a className="text-light" href="https://twitter.com/prp1277" >Twitter</a></button>
