87fadc7f9c254c16ad2fc0eb673448f923bb221f
Patrick Powell <prp1277@gmail.com>
2019-06-19 19:19:14 -0500

    Measurable bitly links

--- src/components/SocialMedia.jsx
+++ src/components/SocialMedia.jsx
@@ -10,15 +10,10 @@ class SocialMedia extends Component {
           <div className="container-fluid py-2">
             <button id="linkedin" className="btn btn-primary mx-2"><a className="text-light" href="https://bit.ly/powell-linkedin" >LinkedIn</a></button>
             <button id="resume" className="btn btn-primary mx-2"><a className="text-light" href="https://1drv.ms/w/s!AoLkNBOSNnKylb1v8s3pCQn2LR-f-g" >Resume</a></button>
-            <button id="github" className="btn btn-primary mx-2"><a className="text-light" href="https://github.com/prp1277" >GitHub</a></button>
-            <button id="twitter" className="btn btn-primary mx-2"><a className="text-light" href="https://twitter.com/prp1277" >Twitter</a></button>
-            <button id="spotify" className="btn btn-primary mx-2"><a className="text-light" href="https://open.spotify.com/user/12180741876?si=jS01F2ouTP-3i2WlRFgIow" >Spotify</a></button>
-            {/**
-            @TODO add spotify component
-            @body See SocialMedia component
-            <Spotify />
-            */}
-            <button id="reddit" className="btn btn-primary mx-2"><a className="text-light" href="https://reddit.com/u/patp0w" >Reddit</a></button>
+            <button id="github" className="btn btn-primary mx-2"><a className="text-light" href="https://bit.ly/Powell-GitHub" >GitHub</a></button>
+            <button id="twitter" className="btn btn-primary mx-2"><a className="text-light" href="https://bit.ly/Powell-Twitter" >Twitter</a></button>
+            <button id="spotify" className="btn btn-primary mx-2"><a className="text-light" href="http://bit.ly/Powell-Spotify" >Spotify</a></button>
+            <button id="reddit" className="btn btn-primary mx-2"><a className="text-light" href="http://bit.ly/Powell-Reddit" >Reddit</a></button>
           </div>
 
         </div>
