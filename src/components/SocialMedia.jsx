dbd242fc231eeacb5fac1d1549002728a9861074
Patrick Powell <prp1277@gmail.com>
2019-07-08 18:20:46 -0500

    replace resume with dev.to

--- src/components/SocialMedia.jsx
+++ src/components/SocialMedia.jsx
@@ -9,10 +9,10 @@ class SocialMedia extends Component {
         <div className="row">
           <div className="container-fluid py-2">
             <button id="linkedin" className="btn btn-primary mx-2"><a className="text-light" href="https://bit.ly/powell-linkedin" >LinkedIn</a></button>
-            <button id="resume" className="btn btn-primary mx-2"><a className="text-light" href="https://1drv.ms/w/s!AoLkNBOSNnKylb1v8s3pCQn2LR-f-g" >Resume</a></button>
             <button id="github" className="btn btn-primary mx-2"><a className="text-light" href="https://bit.ly/Powell-GitHub" >GitHub</a></button>
             <button id="twitter" className="btn btn-primary mx-2"><a className="text-light" href="https://bit.ly/Powell-Twitter" >Twitter</a></button>
             <button id="spotify" className="btn btn-primary mx-2"><a className="text-light" href="http://bit.ly/Powell-Spotify" >Spotify</a></button>
+            <button id="dev" className="btn btn-primary mx-2"><a className="text-light" href="https://dev.to/prp1277">Dev.To</a></button>
             <button id="reddit" className="btn btn-primary mx-2"><a className="text-light" href="http://bit.ly/Powell-Reddit" >Reddit</a></button>
           </div>
         </div>
