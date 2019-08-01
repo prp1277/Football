81d292fdeba9bdf6900018d37dadeafd6dfdbe7f
Patrick Powell <prp1277@gmail.com>
2019-07-31 22:07:29 -0500

    update dir

--- src/App.js
+++ src/App.js
@@ -14,42 +14,32 @@ class App extends Component {
           <SocialMedia />
         </div>
 
-        <div><Disclaimer /></div>
 
         <div id="Page" className="container-fluid col-10 mx-auto">
-          <div>
-
-            <div className="card my-3 text-center">
-              <h4 className="card-header">
-                Alex's Wedding
+          <div id="disclaimer"><Disclaimer /></div>
+          <div className="card my-3 text-center">
+            <h4 className="card-header">
+              Alex's Wedding
             </h4>
 
-              <div className="card-img mx-2 my-1">
-                <img src="https://avatars0.githubusercontent.com/u/36834793?s=460&v=4" alt="" />
-              </div>
+            <div className="card-img mx-2 my-1">
+              <img src="https://avatars0.githubusercontent.com/u/36834793?s=460&v=4" alt="" />
+            </div>
 
-              <div className="card-body">
-
-              </div>
+            <div className="card-body">
 
             </div>
-            <Spotify></Spotify>
 
           </div>
+          <Spotify></Spotify>
 
-          {/* <div className="media container-fluid my-2">
-              <div className="media-body">
-                <img className="media-object" src="https://avatars0.githubusercontent.com/u/36834793?s=460&v=4" alt="" />
-                <h4 className="media-title">Patrick Powell</h4>
-                <p className="media-content">Celebrating Alex's wedding day</p>
-              </div>
-            </div> */}
         </div>
 
         <div id="Footer" className="navbar navbar-dark bg-dark">
           <a href="/"><button className="btn btn-link">Home</button></a>
         </div>
       </div>
+
     )
   }
 }
--- src/components/spotify.js
+++ src/components/spotify.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Spotify = () => {
   return (<div className="text-center">
-    <iframe src="https://open.spotify.com/embed/playlist/43VlEbjj3MeDvAblpakjpd" width="300" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
+    <iframe title="rapMusic" src="https://open.spotify.com/embed/playlist/43VlEbjj3MeDvAblpakjpd" width="300" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
   </div>
   )
 };
