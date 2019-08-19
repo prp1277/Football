02fd00f8f1685eaaba10c136ebbe7ffbd4fea8b5
Patrick Powell <prp1277@gmail.com>
2019-08-19 10:16:30 -0500

    update entry point & don't ignore /build

---
+++ build/precache-manifest.a589315256898fdf13905d6f590de5e5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06649e2c2733419825a6ad0d466a493c",
+    "url": "/prp1277/patrick-powell/index.html"
+  },
+  {
+    "revision": "b641f55fc870f5992cf3",
+    "url": "/prp1277/patrick-powell/static/css/main.e39b9f39.chunk.css"
+  },
+  {
+    "revision": "9634a2804d5597ac62b3",
+    "url": "/prp1277/patrick-powell/static/js/2.548e0644.chunk.js"
+  },
+  {
+    "revision": "b641f55fc870f5992cf3",
+    "url": "/prp1277/patrick-powell/static/js/main.caa2741f.chunk.js"
+  },
+  {
+    "revision": "e7450c88690a0014e112",
+    "url": "/prp1277/patrick-powell/static/js/runtime~main.28486280.js"
+  }
+]);
