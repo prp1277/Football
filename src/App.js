d622251b0652b60661b3ab9c9cd846c0d0c0692e
Patrick Powell <prp1277@gmail.com>
2019-09-10 16:11:17 -0500

    Remove deps

--- src/App.js
+++ src/App.js
@@ -2,17 +2,19 @@ import React from 'react';
 import SocialMedia from './components/SocialMedia';
 import Disclaimer from './components/Disclaimer';
 import './styles.css';
+import nlmForm from './components/nlmForm';
 
 
 function App() {
   return (
-    <div id="App">
+    <div id="root">
 
       <div id="Header" className="navbar navbar-dark bg-dark justify-content-center shadow-lg">
+        <a href="/" className="text-light btn btn-link" >Home</a>
         <SocialMedia />
       </div>
 
-      <div id="Page" className="container-fluid col-10 mx-auto">
+      <div id="Page" className="container col-10">
 
         <div id="disclaimer" className="my-2 shadow-sm"><Disclaimer /></div>
 
@@ -20,17 +22,17 @@ function App() {
           <h1>Hi there</h1>
           <p>My name is Patrick Powell and this is my website.</p>
 
-          <div className="card-img text-center mx-auto my-1">
+
+          {/* <div className="card-img text-center mx-auto my-1">
             <iframe title="powerbi" width="933" height="700" src="https://app.powerbi.com/view?r=eyJrIjoiNzNmZjc2ODEtMDE3OC00NTQxLWIzZmItMWYyNDk4Y2MxYzVlIiwidCI6ImM5ZjZiMDRkLWZlNDYtNGJhMS1iZDNlLWE4ODU5MzIyNjJkOSIsImMiOjJ9"></iframe>
-          </div>
+          </div> */}
 
           <div className="card-body">
-
+            <nlmForm />
           </div>
         </div>
 
         <br />
-
 
       </div>
 
--- src/functions/cloudmersiveNLP.js
+++ src/functions/cloudmersiveNLP.js
@@ -1,23 +1,61 @@
-import { ApiClient, ExtractEntitiesStringApi } from 'cloudmersive-nlp-api-client';
-/**
- * @param baseURL - api.cloudmersive.com
- */
-var defaultClient = ApiClient.instance;
-// Configure API key authorization: Apikey
+var CloudmersiveNlpApiClient = require('cloudmersive-nlp-api-client');
+var defaultClient = CloudmersiveNlpApiClient.ApiClient.instance;
+
+//api.cloudmersive.com
 // Cloudmersive Api Key: 1f0b42fb-af4d-4210-81b6-c18a30aa0a7f
+// Configure API key authorization: Apikey
+
 var Apikey = defaultClient.authentications['Apikey'];
 Apikey.apiKey = "1f0b42fb-af4d-4210-81b6-c18a30aa0a7f"
 
-var api = new ExtractEntitiesStringApi()
+var apiInstance = new CloudmersiveNlpApiClient.WordsApi();
 
-var value = "value_example"; // {String} Input string
+var input = (`${PARSABLE_STRING}`); // String | Input string
 
-
-var callback = function (error, data, response) {
+var wordsPostCallback = function (error, data, response) {
   if (error) {
     console.error(error);
   } else {
     console.log('API called successfully. Returned data: ' + data);
   }
 };
-api.extractEntitiesStringPost(value, callback);
+apiInstance.wordsPost(input, wordsPostCallback);
+
+var wordsAdverbsCallback = function (error, data, response) {
+  if (error) {
+    console.error(error);
+  } else {
+    console.log('API called successfully. Returned data: ' + data);
+  }
+};
+apiInstance.wordsAdverbs(input, wordsAdverbsCallback);
+
+var wordsPronounsCallback = function (error, data, response) {
+  if (error) {
+    console.error(error);
+  } else {
+    console.log('API called successfully. Returned data: ' + data);
+  }
+};
+apiInstance.wordsPronouns(input, wordsPronounsCallback);
+
+var wordsAdjectivesCallback = function (error, data, response) {
+  if (error) {
+    console.error(error);
+  } else {
+    console.log('API called successfully. Returned data: ' + data);
+  }
+};
+apiInstance.wordsAdjectives(input, wordsAdjectivesCallback);
+
+// From https://stackoverflow.com/questions/25284778/converting-plain-text-into-json
+//
+// var PARSABLE_STRING = document.querySelector('#id').innerHTML.split('\n');
+// var arr = [];
+// for (var i = 1, l = text.length - 1; i < l; i++) {
+//     var obj = {};
+//     obj[text[i]] = i
+//     arr.push(obj);
+// }
+
+// console.log(JSON.stringify(arr));
