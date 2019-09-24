c565cbb02fd453cc3369fd29e87617cd4ab61695
Patrick Powell <prp1277@gmail.com>
2019-09-24 12:50:48 -0500

    Add PBI report back in & delete components

--- src/App.js
+++ src/App.js
@@ -1,39 +1,22 @@
 import React from 'react';
 import SocialMedia from './components/SocialMedia';
-import Disclaimer from './components/Disclaimer';
-import Form from './components/inputForm'
 import './styles.css';
 
 
 const App = () => {
   return (
-    <div id="root">
+    <div id="App">
 
-      <div id="Header" className="navbar navbar-dark bg-dark justify-content-center shadow-lg">
+      <div id="Header" className="navbar navbar-dark bg-dark justify-content-center">
         <a href="/" className="text-light btn btn-link" >Home</a>
         <SocialMedia />
       </div>
 
-      <div id="Page" className="container col-10">
-
-        <Disclaimer />
-
-        <div className="card bg-light my-3 py-2 text-center shadow-sm">
-          <h1>Hi there</h1>
-          <p>My name is Patrick Powell and this is my website.</p>
-
-
-          {/* <div className="card-img text-center mx-auto my-1">
-            <iframe title="powerbi" width="933" height="700" src="https://app.powerbi.com/view?r=eyJrIjoiNzNmZjc2ODEtMDE3OC00NTQxLWIzZmItMWYyNDk4Y2MxYzVlIiwidCI6ImM5ZjZiMDRkLWZlNDYtNGJhMS1iZDNlLWE4ODU5MzIyNjJkOSIsImMiOjJ9"></iframe>
-          </div> */}
-
-          <Form />
-        </div>
-
-
-        <br />
-
-      </div>
+      <section className="container col-10 my-3 bg-light shadow-lg text-center">
+        <h3>There isn't much to see here</h3>
+        <p>As a placeholder, here's a PowerBI report showing some information from GitHubs API.</p>
+        <iframe title="GitHubStats" width="933" height="700" src="https://app.powerbi.com/view?r=eyJrIjoiNzNmZjc2ODEtMDE3OC00NTQxLWIzZmItMWYyNDk4Y2MxYzVlIiwidCI6ImM5ZjZiMDRkLWZlNDYtNGJhMS1iZDNlLWE4ODU5MzIyNjJkOSIsImMiOjJ9" frameborder="0" allowFullScreen="true"></iframe>
+      </section>
 
       <div id="Footer" className="navbar navbar-dark bg-dark">
         <a className="text-light btn-link mx-2" href="/">Home</a>
--- src/components/Disclaimer.jsx
+++ src/components/Disclaimer.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 
-const Disclaimer = () => {
-  return (
-    <div class="alert alert-warning alert-dismissible fade show">
-      <button type="button" class="close" data-dismiss="alert">&times;</button>
-      <p>We and our partners use cookies to personalize your experience, to show you ads based on your interests, and for measurement and analytics purposes. By using our website and our services, you agree to our use of cookies as described in our Cookie Policy.</p>
-      {/* <strong>Warning!</strong> This alert box could indicate a warning that might need attention. */}
-    </div>
-
-  )
+class Disclaimer extends React.Component {
+  render() {
+    return (
+      <div className="alert alert-warning alert-dismissible fade show">
+        <button type="button" className="close" data-dismiss="alert">&times;</button>
+        <p>We and our partners use cookies to personalize your experience, to show you ads based on your interests, and for measurement and analytics purposes. By using our website and our services, you agree to our use of cookies as described in our Cookie Policy.</p>
+        {/* <strong>Warning!</strong> This alert box could indicate a warning that might need attention. */}
+      </div>
+    )
+  }
 };
 export default Disclaimer;
